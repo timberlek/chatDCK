5bdef92c3b23cacedab670f666bc1873357a19c3
Jules Chuzeville <jules.chuzeville@gmail.com>
2023-04-12 10:16:37 +0200

    aa

--- src/app/ClientThread.java
+++ src/app/ClientThread.java
@@ -13,6 +13,17 @@ public class ClientThread extends Thread{
     private PrintWriter out;
     private Server server;
 
+	private String username;
+	private String password;
+
+    public ClientThread(Socket socket, String username, String password, Server server) {
+        this.clientSocket = socket;
+		this.username     = username;
+		this.password     = password;
+		this.server       = server;
+    }
+
+
     public ClientThread(Socket socket, Server server) {
         this.clientSocket = socket;
         this.server = server;
@@ -22,6 +33,28 @@ public class ClientThread extends Thread{
         try {
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream(), true);
+			Scanner sc = new Scanner(System.in);
+
+			if(this.username == null)
+			{
+				System.out.println("Username :");
+				this.setUsername(sc.nextLine());
+			}
+			
+			
+			if(this.password == null)
+			{
+				System.out.println("Nouveau password :");
+				this.setPassword(sc.nextLine());
+			}
+			else
+			{
+				System.out.println("Password :");
+				if (sc.nextLine() != this.password)
+				{
+					break;
+				}
+			}
 
             out.println("Bienvenue sur le serveur de chat !");
 
@@ -30,14 +63,30 @@ public class ClientThread extends Thread{
                 if (inputLine == null) {
                     break;
                 }
-                System.out.println("Message recu de " + clientSocket.getInetAddress().getHostAddress() + ": " + inputLine);
+                System.out.println("Message recu de " + this.getUsername() + " : " + inputLine);
                 server.sendMsg(inputLine);
             }
 
-            System.out.println("Connexion fermee par " + clientSocket.getInetAddress().getHostAddress());
+            System.out.println("Connexion fermee par " + this.getUsername());
             clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
