f82f14e86665ff2e09673a209e5185e84a772e1d
Ledoux Gaétan <41428973+gaetanldx94@users.noreply.github.com>
2023-04-12 11:56:05 +0200

    Update Server.java

--- src/app/Server.java
+++ src/app/Server.java
@@ -1,33 +1,47 @@
 package src.app;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
 
+
 public class Server
 {
-	private LinkedList<Socket> threads;
+	private LinkedList<ClientThread> threads;
 
 	public Server(int port) throws IOException {
 		ServerSocket serverSocket = new ServerSocket(port);
-		threads = new LinkedList<Socket>();
+		threads = new LinkedList<ClientThread>();
 
 		System.out.println("Le serveur de chat est en ligne sur le port 9000...");
 
 		while (true) {
 			Socket clientSocket = serverSocket.accept();
 			System.out.println("Nouvelle connexion de " + clientSocket.getInetAddress().getHostAddress());
-			threads.add(clientSocket);
-			new ClientThread(clientSocket, this).start();
+
+            //Recup pseudo
+            String pseudo="";
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+			out.println("Rentrez votre pseudo : ");
+			pseudo = in.readLine();
+
+			threads.add( new ClientThread(clientSocket, this, pseudo));
+            threads.getLast().start();
+			
 		}
 	}
 
 	public void sendMsg(String msg) throws IOException {
-		for(Socket client : threads) {
-			if(!client.isClosed()) {
-				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+		for(ClientThread client : threads) {
+			if(!client.getSocket().isClosed()) {
+				PrintWriter out = new PrintWriter(client.getSocket().getOutputStream(), true);
 				out.println(msg);
 			}
 		}
