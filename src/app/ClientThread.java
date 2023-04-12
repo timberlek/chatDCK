32943d7d9aea84a3fb1f61a260628fa6c78bce22
Ledoux Gaétan <41428973+gaetanldx94@users.noreply.github.com>
2023-04-12 11:56:27 +0200

    Update ClientThread.java

--- src/app/ClientThread.java
+++ src/app/ClientThread.java
@@ -5,33 +5,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientThread extends Thread{
 
 	private Socket clientSocket;
+    private String pseudo;
     private BufferedReader in;
     private PrintWriter out;
     private Server server;
 
-    public ClientThread(Socket socket, Server server) {
+    public ClientThread(Socket socket, Server server,String pseudo) 
+    {
         this.clientSocket = socket;
         this.server = server;
+        this.pseudo = pseudo;
     }
 
     public void run() {
+
+		boolean bool = true;
+
         try {
+
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream(), true);
+			Scanner sc = new Scanner(System.in);
+			
 
             out.println("Bienvenue sur le serveur de chat !");
 
-            while (true) {
+            while (bool) {
                 String inputLine = in.readLine();
                 if (inputLine == null) {
                     break;
                 }
-                System.out.println("Message recu de " + clientSocket.getInetAddress().getHostAddress() + ": " + inputLine);
-                server.sendMsg(inputLine);
+                System.out.println("Message recu de " + clientSocket.getInetAddress().getHostAddress() + " : " + inputLine);
+                server.sendMsg(this.pseudo + ": " + inputLine);
             }
 
             System.out.println("Connexion fermee par " + clientSocket.getInetAddress().getHostAddress());
@@ -40,4 +50,7 @@ public class ClientThread extends Thread{
             e.printStackTrace();
         }
     }
+
+    public Socket getSocket() { return this.clientSocket; }
+
 }
