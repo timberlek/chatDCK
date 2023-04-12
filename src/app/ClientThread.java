39a381b3ecfc152b7893085ee64311dcaf57c2bc
Ledoux Gaétan <41428973+gaetanldx94@users.noreply.github.com>
2023-04-12 10:10:58 +0200

    Add files via upload

--- src/app/ClientThread.java
+++ src/app/ClientThread.java
@@ -5,87 +5,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class ClientThread extends Thread{
 
 	private Socket clientSocket;
     private BufferedReader in;
     private PrintWriter out;
+    private Server server;
 
-	private String username;
-	private String password;
-
-    public ClientThread(Socket socket, String username, String password) {
+    public ClientThread(Socket socket, Server server) {
         this.clientSocket = socket;
-		this.username     = username;
-		this.password     = password;
-    }
-
-	public ClientThread(Socket socket) {
-        this.clientSocket = socket;
+        this.server = server;
     }
 
     public void run() {
         try {
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new PrintWriter(clientSocket.getOutputStream(), true);
-			Scanner sc = new Scanner(System.in);
 
-			if(this.username == null)
-			{
-				System.out.println("Username :");
-				this.setUsername(sc.nextLine());
-			}
-			
-			
-			if(this.password == null)
-			{
-				System.out.println("Nouveau password :");
-				this.setPassword(sc.nextLine());
-			}
-			else
-			{
-				System.out.println("Password :");
-				if (sc.nextLine() != this.password)
-				{
-					break;
-				}
-			}
-
-			
             out.println("Bienvenue sur le serveur de chat !");
 
             while (true) {
                 String inputLine = in.readLine();
-                if (inputLine.equals("STOP")) {
+                if (inputLine == null) {
                     break;
                 }
-                System.out.println("Message recu de " + this.getUsername() + " : " + inputLine);
+                System.out.println("Message recu de " + clientSocket.getInetAddress().getHostAddress() + ": " + inputLine);
+                server.sendMsg(inputLine);
             }
 
-            System.out.println("Connexion fermee par " + this.getUsername());
+            System.out.println("Connexion fermee par " + clientSocket.getInetAddress().getHostAddress());
             clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	
 }
--- src/app/Server.java
+++ src/app/Server.java
@@ -1,22 +1,35 @@
 package src.app;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import src.app.ClientThread;
+import java.util.LinkedList;
 
 public class Server
 {
+	private LinkedList<Socket> threads;
+
 	public Server(int port) throws IOException {
 		ServerSocket serverSocket = new ServerSocket(port);
+		threads = new LinkedList<Socket>();
 
 		System.out.println("Le serveur de chat est en ligne sur le port 9000...");
 
 		while (true) {
 			Socket clientSocket = serverSocket.accept();
 			System.out.println("Nouvelle connexion de " + clientSocket.getInetAddress().getHostAddress());
-			new ClientThread(clientSocket).start();
+			threads.add(clientSocket);
+			new ClientThread(clientSocket, this).start();
+		}
+	}
+
+	public void sendMsg(String msg) throws IOException {
+		for(Socket client : threads) {
+			if(!client.isClosed()) {
+				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+				out.println(msg);
+			}
 		}
 	}
 }
