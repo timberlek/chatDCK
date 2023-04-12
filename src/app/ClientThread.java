ee21987f11ccb943c4e4d549a8a841234d6deeb5
Jules Chuzeville <jules.chuzeville@gmail.com>
2023-04-12 10:17:10 +0200

    aa

--- src/app/ClientThread.java
+++ src/app/ClientThread.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientThread extends Thread{
 
